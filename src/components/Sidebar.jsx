647e25ff20499e9f137625feac85329c66108064
Aman Singh <singhaman00429@gmail.com>
2024-08-27 20:25:30 +0530

    first commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -25,7 +25,7 @@ const Sidebar = () => {
           </div>
 
         </> : <>
-          <div className="min-w-16 bottom-0 h-screen bg-slate-50 sticky top-0 rounded-2xl ">
+          <div className="min-w-16 bottom-0 h-screen bg-slate-50 sticky top-0 max-sm:hidden rounded-2xl ">
             <button
               onClick={() => setSideActive((prev) => prev = !prev)}
               className="text-4xl w-full font-semibold hover:-translate-y-1 hover:scale-110 transition hover:text-accentblue text-textDark"
